1cc51fda5863214cd198fe8620b53b8817c501a8
Talha Gaffar <talhaansari78670@gmail.com>
2019-07-26 19:58:18 +0530

    assignment2 firstREST-API

---
+++ models/leaders.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+
+
+const leaderSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    abbr: {
+        type: String,
+        default: ''
+    },
+    designation: {
+        type: String,
+        required: true,
+        min: 0
+    },
+    featured: {
+        type: Boolean,
+        default:false      
+    }
+});
+
+
+
+var Leaders = mongoose.model('Leader',leaderSchema);
+
+module.exports = Leaders;
+
+
+
+
+
+
